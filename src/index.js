7b1577687eccfe53d8f7f578a73d240d0f4f4912
Ranjan Shrestha <ranjan@sharingapples.com>
2017-04-04 22:36:45 +0545

    Publish project with ES5 compatibility
    While testing apps with Jest, the react-native-transition's import
    weren't being recognized. So, added babel to transpile using
    react-native presets.

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import createTransition from './Transition';
+import Fade from './transitions/Fade';
+import FlipX from './transitions/FlipX';
+import FlipY from './transitions/FlipY';
+import SlideLeft from './transitions/SlideLeft';
+import SlideRight from './transitions/SlideRight';
+import SlideUp from './transitions/SlideUp';
+import SlideDown from './transitions/SlideDown';
+import transitionStylePropType from './transitionStylePropType';
+
+export {
+  createTransition,
+  transitionStylePropType,
+
+  Fade,
+  FlipX,
+  FlipY,
+  SlideLeft,
+  SlideRight,
+  SlideUp,
+  SlideDown,
+};
